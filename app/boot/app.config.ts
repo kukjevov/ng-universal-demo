dd7b4cc8ae4c63605b709b4054da2e1a08b478be
marek.bukovsky <marek.bukovsky@asseco-ce.com>
2019-06-19 09:04:29 +0200

    INT: now removed timeout for notificatiosn

--- app/boot/app.config.ts
+++ app/boot/app.config.ts
@@ -1,6 +1,21 @@
+import {FactoryProvider} from '@angular/core';
+import {NOTIFICATIONS_OPTIONS, NotificationsOptions} from '@ng/notifications';
+
+export function factory(): NotificationsOptions<any, any>
+{
+    return {
+        timeout: 0
+    };
+}
+
 /**
  * Array of providers that are used in app module
  */
 export var providers =
 [
+    <FactoryProvider>
+    {
+        provide: NOTIFICATIONS_OPTIONS,
+        useFactory: factory
+    }
 ];
