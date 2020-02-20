3b379b8f7f962034ca13f9eaf153583f59f3d959
kukjevov <kukjevov@gmail.com>
2020-02-20 08:48:35 +0100

    INT: updated gulp to version 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,11 @@
-const gulp = require('gulp'),
+const {src, dest, series} = require('gulp'),
       sass = require('gulp-sass'),
-      watch = require('gulp-watch'),
       through2 = require('through2'),
       gitVersion = require('@kukjevov/gulp-git-version');
 
 function logCopied()
 {
-    return through2.obj(function(vinyl, enc, callback)
+    return through2.obj(function(vinyl, _enc, callback)
     {
         console.log("Copying follow file: '" + vinyl.path + "'");
         this.push(vinyl);
@@ -15,37 +14,21 @@ function logCopied()
     });
 }
 
-gulp.task("copy-config", function()
+function copyConfig()
 {
-    return gulp.src("config/i18n/**/*.json")
+    return src("config/i18n/**/*.json")
         .pipe(logCopied())
-        .pipe(gulp.dest("wwwroot/config/i18n"));
-});
+        .pipe(dest("wwwroot/config/i18n"));
+}
 
-gulp.task("watch-config", ["copy-config"], function()
+function compileScss()
 {
-    return watch('config/i18n/**/*.json', { events: ['change', 'add'] })
-        .pipe(through2.obj(function(vinyl, enc, cb)
-                           {
-                               if(vinyl.event == 'change' || vinyl.event == 'add')
-                               {
-                                   console.log("File '" + vinyl.basename + "' is being copied.");
-                               }
-
-                               this.push(vinyl);
-                               cb();
-                           }))
-        .pipe(gulp.dest('wwwroot/config/i18n'));
-});
-
-gulp.task("compile-scss", function()
-{
-    return gulp.src('content/*.scss')
+    return src('content/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('wwwroot/content'));
-});
+        .pipe(dest('wwwroot/content'));
+};
 
-gulp.task("prepare-version", function(cb)
+function prepareVersion(cb)
 {
     gitVersion(
     {
@@ -65,14 +48,15 @@ gulp.task("prepare-version", function(cb)
             suffix: "beta"
         }
     }, cb);
-});
+};
 
-gulp.task("build",
-          ["copy-config",
-           "prepare-version"],
-          function(cb)
-{
-    console.log("Gulp build has finished");
+const build = series(copyConfig,
+                     prepareVersion,
+                     function buildComplete(cb)
+                     {
+                         console.log("Gulp build has finished");
+                         cb();
+                     });
 
-    cb();
-});
+exports.build = build;
+exports.compileScss = compileScss;
