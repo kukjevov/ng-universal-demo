b4a016275c4d1970bada183edd4ef56d7c6abb35
kukjevov <kukjevov@gmail.com>
2022-12-23 10:00:21 +0100

    INT: updated packages to newest version

--- app/modules/formsFeature.module.ts
+++ app/modules/formsFeature.module.ts
@@ -3,13 +3,19 @@ import {ReactiveFormsModule} from '@angular/forms';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {NgSelectEditModule, NgSelectModule} from '@anglr/select';
 import {NumberInputModule, HasErrorModule, FormPipesModule} from '@anglr/common/forms';
-import {DateTimeSelectorModule} from '@anglr/datetime';
+import {DatePickerInputSADirective, DateTimeModule, DateTimePickerModule, WithTimeSADirective, WithTodaySADirective} from '@anglr/datetime';
 
 /**
  * Common module for enabling forms features
  */
 @NgModule(
 {
+    imports:
+    [
+        WithTimeSADirective,
+        WithTodaySADirective,
+        DatePickerInputSADirective,
+    ],
     exports:
     [
         ReactiveFormsModule,
@@ -17,9 +23,13 @@ import {DateTimeSelectorModule} from '@anglr/datetime';
         NumberInputModule,
         NgSelectModule,
         NgSelectEditModule,
-        DateTimeSelectorModule,
         HasErrorModule,
         FormPipesModule,
+        DateTimeModule,
+        DateTimePickerModule,
+        WithTimeSADirective,
+        WithTodaySADirective,
+        DatePickerInputSADirective,
     ]
 })
 export class FormsFeatureModule
