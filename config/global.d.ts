7b14d15c2fe9bc26aba3cacfe91cd1a19ccabb67
kukjevov <kukjevov@gmail.com>
2023-05-12 13:39:58 +0200

    INT: updated git config for project

--- config/global.d.ts
+++ config/global.d.ts
@@ -3,7 +3,8 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="../node_modules/@anglr/common/typings/positions/index.d.ts" />
 
-declare let isProduction: boolean;
+declare const isProduction: boolean;
+declare const isNgsw: boolean;
 
 interface ImportMeta
 {
