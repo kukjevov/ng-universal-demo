534366a298dff44487c8b3cb33ece074121f1f84
kukjevov <kukjevov@gmail.com>
2023-09-14 08:02:13 +0200

    INT: removed unused modules

--- app/boot/server-app.module.ts
+++ app/boot/server-app.module.ts
@@ -1,43 +1,43 @@
-import {NgModule, FactoryProvider} from '@angular/core';
-import {ServerModule} from '@angular/platform-server';
-import {AnglrExceptionHandlerOptions} from '@anglr/error-handling';
-import {ServerProvidersModule} from '@anglr/server-stuff';
-import {ServerHotkeysModule} from '@anglr/server-stuff/hotkeys';
+// import {NgModule, FactoryProvider} from '@angular/core';
+// import {ServerModule} from '@angular/platform-server';
+// import {AnglrExceptionHandlerOptions} from '@anglr/error-handling';
+// import {ServerProvidersModule} from '@anglr/server-stuff';
+// import {ServerHotkeysModule} from '@anglr/server-stuff/hotkeys';
 
-import {AppComponent} from './app.component';
-import {AppModule} from './app.module';
-import {config} from '../config';
+// import {AppComponent} from './app.component';
+// import {AppModule} from './app.module';
+// import {config} from '../config';
 
-/**
- * Factory for AnglrExceptionHandlerOptions
- */
-export function anglrExceptionHandlerOptionsFactory()
-{
-	return new AnglrExceptionHandlerOptions(config.configuration.debug, false);
-}
+// /**
+//  * Factory for AnglrExceptionHandlerOptions
+//  */
+// export function anglrExceptionHandlerOptionsFactory()
+// {
+// 	return new AnglrExceptionHandlerOptions(config.configuration.debug, false);
+// }
 
-/**
- * Entry module for server side
- */
-@NgModule(
-{
-    bootstrap: [AppComponent],
-    imports: 
-    [
-        AppModule,
-        ServerModule,
-        ServerProvidersModule,
-        ServerHotkeysModule
-    ],
-    providers:
-    [
-        <FactoryProvider>
-        {
-            provide: AnglrExceptionHandlerOptions,
-            useFactory: anglrExceptionHandlerOptionsFactory
-        }
-    ]
-})
-export class ServerAppModule 
-{
-}
+// /**
+//  * Entry module for server side
+//  */
+// @NgModule(
+// {
+//     bootstrap: [AppComponent],
+//     imports: 
+//     [
+//         AppModule,
+//         ServerModule,
+//         ServerProvidersModule,
+//         ServerHotkeysModule
+//     ],
+//     providers:
+//     [
+//         <FactoryProvider>
+//         {
+//             provide: AnglrExceptionHandlerOptions,
+//             useFactory: anglrExceptionHandlerOptionsFactory
+//         }
+//     ]
+// })
+// export class ServerAppModule 
+// {
+// }
