9d27b948da3f0cbe609927610c31ec41e84f87fd
kukjevov <kukjevov@gmail.com>
2024-05-09 09:23:41 +0200

    INT: another cleanup in project

--- app/main.server.ts
+++ app/main.server.ts
@@ -1,12 +1,5 @@
 /* eslint-disable ressurectit/imports-spacing */
 /* eslint-disable ressurectit/imports-order */
-import xhr2 from 'xhr2';
-
-// //HACK - enables setting cookie header
-xhr2.prototype._restrictedHeaders.cookie = false;
-xhr2.prototype._restrictedHeaders.cookie2 = false;
-
-// import 'form-data';
 import '../config/configServerOverride';
 import './server.pollyfils';
 import {enableProdMode} from '@angular/core';
