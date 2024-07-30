24e0caa58e9ed1c3552f3995ad5313946b2b0d0c
kukjevov <kukjevov@gmail.com>
2024-07-30 09:27:02 +0200

    INT: updated basic layout

--- app/pages/+default/home/home.component.ts
+++ app/pages/+default/home/home.component.ts
@@ -2,6 +2,7 @@ import {Component, ChangeDetectionStrategy} from '@angular/core';
 import {ComponentRedirectRoute} from '@anglr/common/router';
 import {Authorize, ComponentRouteAuthorized} from '@anglr/authentication';
 import {DebugDataCopyClickModule} from '@anglr/common/material';
+import {WithScrollableCssClass} from '@anglr/common';
 
 /**
  * Home component
@@ -20,6 +21,7 @@ import {DebugDataCopyClickModule} from '@anglr/common/material';
 @ComponentRedirectRoute('', 'home')
 @ComponentRouteAuthorized({path: 'home'})
 @Authorize('home-page')
+@WithScrollableCssClass()
 export class HomeComponent
 {
 }
